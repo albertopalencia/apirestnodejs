cb53b66647e2252bb187bd64d8baa67f2b5fbf28
unknown <apalenciabenedetti@gmail.com>
2019-06-25 08:06:06 -0500

    cambio en el api

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -32,5 +32,11 @@ let usuarioSchema = new Schema({
     }
 });
 
+usuarioSchema.methods.toJSON = function(){
+    let user = this;
+    let usuarioObject = user.toObject();
+    delete usuarioObject.password;
+    return usuarioObject;
+}
 
 module.exports = mongoose.model('Usuario', usuarioSchema);
